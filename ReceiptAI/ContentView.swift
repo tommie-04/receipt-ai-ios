10dd9b548cd66d9568acd5a586e3ea5831c47d9d
Tommie <leung0404@outlook.com>
2026-07-21 21:14:48 -0700

    Initial Commit

---
+++ ReceiptAI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ReceiptAI
+//
+//  Created by Tommie on 2026-07-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ReceiptAI/ReceiptAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReceiptAIApp.swift
+//  ReceiptAI
+//
+//  Created by Tommie on 2026-07-21.
+//
+
+import SwiftUI
+
+@main
+struct ReceiptAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
